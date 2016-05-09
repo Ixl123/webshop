10f841930e46ae99efb605ef1980445d526864b8
Kevin Dietrich <shadow.developer@online.de>
2016-05-09 14:05:33 +0200

    Added first test function for rest call. Deleted unnecessary code.

--- src/main/java/hska/iwi/eShopMaster/microservices/accounts/AccountsController.java
+++ src/main/java/hska/iwi/eShopMaster/microservices/accounts/AccountsController.java
@@ -2,13 +2,17 @@ package hska.iwi.eShopMaster.microservices.accounts;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AccountsController {
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@ResponseBody
 	public String home() {
-		return "index";
+		System.out.println("Hello World!");
+		return "Hello World!";
 	}
 	
 }
--- src/main/java/hska/iwi/eShopMaster/microservices/services/accounts/AccountsServer.java
+++ src/main/java/hska/iwi/eShopMaster/microservices/services/accounts/AccountsServer.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-
 import hska.iwi.eShopMaster.microservices.accounts.AccountsController;
-import hska.iwi.eShopMaster.microservices.accounts.AccountsWebApplication;
 
 
 @EnableAutoConfiguration
 @EnableDiscoveryClient
-@Import(AccountsWebApplication.class)
 public class AccountsServer {
 	
 	public static void main(String[] args) {
